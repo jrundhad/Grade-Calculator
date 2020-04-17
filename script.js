660726adc48a22850d8bf6fbdb5e6f56bec87ac9
Elliot Cho <59032999+elliotcho@users.noreply.github.com>
2020-04-16 20:05:15 -0400

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var size=5;
+var size=0;
 
 $(document).ready(function(){
     generateTable(1, size);
@@ -14,7 +14,27 @@ $(document).ready(function(){
     });
 
     $("#calc").click(function(){
-       
+        if(size==0){alert("Add rows to calculate your grade!")}
+
+        else{
+            var lastChild=$("#container").children().last();
+
+            if(lastChild.text()!="Calculate Grade"){lastChild.remove();}
+
+            var result=compute();
+
+            var output;
+
+            if(result==-1){
+                output="All the grades and weights must be a number between 0 and 100.";
+            }
+            
+            else{
+                output="Your final grade is "+ result;
+            }
+
+            $("#container").append("<h1>"+output+"</h1>");
+        }
     });
 });
 
@@ -22,18 +42,46 @@ function generateTable(low, high){
     for(var i=low;i<=high;i++){
         $("tbody").append("<tr id =" +i + "></tr>");
         $("#" + i).append("<td>" + i + "</td>");
-        $("#" + i).append("<td> <input type='number' min='0' step='any'> </td>");
-        $("#" + i).append("<td> <input type='number' min='0' step='any'> </td>");
+        $("#" + i).append("<td> <input type='number'class='grade"+i+"' min='0' step='any'> </td>");
+        $("#" + i).append("<td> <input type='number'class='weight"+i+"'min='0' step='any'> </td>");
     }
 }
 
-//function is still a work in progress
 function compute(){
-    for(var i=0;i<size;i++){
-        var grade=$("#" + i).children(".grade").text();
-        var weight=$("#" + i).children(".weight").text();
+    var finalGrade=0;
+    var finalWeight=0;
 
-        console.log(grade);
-        console.log(weight);
+    for(var i=1;i<=size;i++){
+        var currWeight=$(".weight" +i).val();
+        var currGrade=$(".grade" +i).val();
+
+        currWeight=validate(currWeight)/100;
+        currGrade=validate(currGrade);
+
+        if(currWeight<0 || currGrade<0){return -1;}
+
+        finalWeight+=currWeight;
+        finalGrade+=currGrade*currWeight;
     }
+
+    if(finalWeight==0){return finalGrade.toFixed(2);}
+
+    return (finalGrade/finalWeight).toFixed(2);
+}
+
+
+function validate(input){
+    if(Number(input)==false && input!=0){
+        return -1;
+    }
+
+    if(Number(input)<0){
+        return -1;
+    }
+
+    if(Number(input)>100){
+        return -1;
+    }
+
+    return Number(input);
 }
